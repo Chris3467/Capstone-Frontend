f3f19b786986312b84c617bb98bd8e85ef18d778
Chris3467 <chris.collado815@gmail.com>
2025-01-17 12:05:21 -0500

    updated

--- src/components/Chessboard.jsx
+++ src/components/Chessboard.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Chessboard } from "react-chessboard";
 import { Chess } from "chess.js";
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,13 @@
-import { Link } from "react-router";
+import { Link } from "react-router"; // Make sure you're using "react-router-dom"
+import { GiChessKnight } from "react-icons/gi";
 
 function Navbar() {
   return (
     <div className="nav">
-      <Link to="/">Home</Link>
+      <Link to="/">
+        <GiChessKnight size={30} style={{ color: "#f0e2c8" }} />{" "}
+        {/* Adjust size and color as needed */}
+      </Link>
       <Link to="/dashboard">Dashboard</Link>
       <Link to="/training">Training</Link>
       <Link to="/profile">Profile</Link>
